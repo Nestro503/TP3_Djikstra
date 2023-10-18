0d1ec7638f6d38402af7c2f63b3595baac0d6e4c
Nestro503 <116881924+Nestro503@users.noreply.github.com>
2023-10-18 13:49:37 +0200

    ajout dernier fichier

---
+++ graphe.h
@@ -0,0 +1,67 @@
+
+#ifndef TP3_DJIKSTRA_GRAPHE_H
+#define TP3_DJIKSTRA_GRAPHE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+// SOURCES : Bien que certaines fonctions ont été ajoutées la plupart des strcutures présentes est propirété
+//              intelectuelle de Mr DEBIZE.
+
+
+/* Structure d'un arc*/
+struct Arc
+{
+    int sommet; // numéro de sommet d'un arc adjacent au sommet initial
+    int valeur;
+    struct Arc* arc_suivant;
+};
+
+/* Alias de pointeur sur un Arc */
+typedef struct Arc* pArc;
+
+/* Structure d'un sommet*/
+struct Sommet
+{
+    struct Arc* arc;
+    int valeur;
+    char couleur;
+    int pred;
+    bool state;
+};
+
+/* Alias de pointeur sur un Sommet */
+typedef struct Sommet* pSommet;
+
+/* Alias d'un Graphe */
+typedef struct Graphe
+{
+    int taille;
+    int ordre;
+    int ppsommet;
+    pSommet* pSommet;
+} Graphe;
+
+
+// créer le graphe
+Graphe* CreerGraphe(int ordre);
+
+/* La construction du réseau peut se faire à partir d'un fichier dont le nom est passé en paramètre
+Le fichier contient : ordre, taille,orientation (0 ou 1)et liste des arcs */
+Graphe * lire_graphe(char * nomFichier, int ppsommet, int force_orientation, int sommet_initial);
+
+void pp_sommet2(int* ppsommet, char * nomFichier);
+
+// Ajouter l'arête entre les sommets s1 et s2 du graphe
+pSommet* CreerArete(pSommet* sommet,int s1,int s2);
+
+/* affichage des successeurs du sommet num*/
+void afficher_successeurs(pSommet * sommet, int num);
+
+
+/*affichage du graphe avec les successeurs de chaque sommet */
+void graphe_afficher(Graphe* graphe);
+
+
+#endif //TP3_DJIKSTRA_GRAPHE_H
